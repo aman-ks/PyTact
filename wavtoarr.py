20e8a98e69825b50e9c4835a82abce5b702e7605
aman <aman@aman.(none)>
2013-10-31 20:58:18 +0530

    Added plotting

--- wavtoarr.py
+++ wavtoarr.py
@@ -1,7 +1,8 @@
-import matplotlib
+import matplotlib.pyplot as plt
 import wave
 import numpy as np
 from scikits.audiolab import Sndfile
+
 
 a = wave.open('output.wav','r')
 total_frames = a.getnframes()
@@ -13,9 +14,20 @@ enc = f.encoding
 format_wave = f.format
 
 data_float = f.read_frames(total_frames,dtype=np.float32)
-data_after = np.fft.fft(data_float)
+data_after_ft = np.fft.fft2(data_float)
+
 
 print fs,nc,enc,format_wave
 print "\n \n"
 print data_float
-print data_after
+print data_float.size, data_float.shape
+print "\n \n"
+print data_after_ft
+
+#plt.figure(1)
+#plt.subplot(211)
+#plt.plot(data_float,'k')
+
+#plt.subplot(212)
+#plt.plot(data_after_ft, 'r')
+#plt.show()
