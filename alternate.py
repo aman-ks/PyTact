8b06aa7ef6709e785763574f44096e57cf34aa13
aman <aman@aman.(none)>
2013-10-31 20:55:58 +0530

    The modified alternate.py

---
+++ alternate.py
@@ -0,0 +1,61 @@
+import math
+import numpy
+from numpy.fft import fft as fft
+from scipy.fftpack import dct
+from scipy.io import wavfile
+
+numCoefficients = 13
+minHz = 0
+maxHz = 22.000
+blockSize = 2048
+
+def find_mfcc(signal):
+    complexSpectrum = fft(signal)
+    powerSpectrum = abs(complexSpectrum) ** 2
+    filteredSpectrum = numpy.dot(powerSpectrum, melFilterBank(len(powerSpectrum)))
+    logSpectrum = numpy.log(filteredSpectrum)
+    dctSpectrum = dct(logSpectrum, type=2)
+
+    return dctSpectrum
+
+def melFilterBank(blockSize):
+    numBands = int(numCoefficients)
+    maxMel = int(freqToMel(maxHz))
+    minMel = int(freqToMel(minHz))
+
+    # Create a matrix for triangular filters, one row per filter
+    filterMatrix = numpy.zeros((numBands, blockSize))
+
+    melRange = numpy.array(xrange(numBands + 2))
+
+    melCenterFilters = melRange * (maxMel - minMel) / (numBands + 1) + minMel
+
+    # each array index represent the center of each triangular filter
+    aux = numpy.log(1 + 1000.0 / 700.0) / 1000.0
+    aux = (numpy.exp(melCenterFilters * aux) - 1) / 22050
+    aux = 0.5 + 700 * blockSize * aux
+    aux = numpy.floor(aux)
+    centerIndex = numpy.array(aux, int)
+
+    for i in xrange(numBands):
+        start, centre, end = centerIndex[i:i + 3]
+        k1 = numpy.float32(centre - start)
+        k2 = numpy.float32(end - centre)
+        up = (numpy.array(xrange(start, centre)) - start) / k1
+        down = (end - numpy.array(xrange(centre, end))) / k2
+
+        filterMatrix[i][start:centre] = up
+        filterMatrix[i][centre:end] = down
+
+    return filterMatrix.transpose()
+
+def freqToMel(freq):
+    return 1127.01048 * math.log(1 + freq / 700.0)
+
+def melToFreq(mel):
+    return 700 * (math.exp(mel / 1127.01048 - 1))
+    
+    
+sampleRate, signal = wavfile.read("output.wav")
+output = find_mfcc(signal)
+print output    
--- wavtoarr.py
+++ wavtoarr.py
@@ -1,27 +1,21 @@
+import matplotlib
 import wave
 import numpy as np
 from scikits.audiolab import Sndfile
 
 a = wave.open('output.wav','r')
 total_frames = a.getnframes()
-#rate = a.getframerate()
 f = Sndfile('output.wav','r')
 
 fs = f.samplerate
 nc = f.channels
 enc = f.encoding
-
 format_wave = f.format
 
-
-
-data_float = f.read_frames(total_frames,dtype=np.float64)
-
-print fs,nc,enc,format_wave
-#print data
-print "\n \n"
-print data_float
-
+data_float = f.read_frames(total_frames,dtype=np.float32)
 data_after = np.fft.fft(data_float)
 
+print fs,nc,enc,format_wave
+print "\n \n"
+print data_float
 print data_after
