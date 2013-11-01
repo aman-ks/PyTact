20e89367d342e5a38c6ca0cd2e1bfb1abcd0b9c6
Aman K Srivastava <eamanshrivastava@gmail.com>
2013-11-01 20:27:56 +0530

    Update FingersCrossed.py

--- FingersCrossed.py
+++ FingersCrossed.py
@@ -2,7 +2,7 @@ import sys
 import urllib
 
 _base_url = 'https://www.google.com/speech-api/v1/recognize?xjerr=1&client=chromium&lang=en-IN&maxresults=1'
-_filepath = '/aman/output.flac'
+_filepath = 'output.flac'
            
 post_data = {'Content_Type' : 'audio/x-flac; rate=16000', 'Content' : open(_filepath, mode="rb") }
 request = urllib.urlopen(_base_url, urllib.urlencode(post_data))
