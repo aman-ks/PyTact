76d96e0c61a56d9c5f79df5f642a6ebc178fbcb4
aman <aman@aman.(none)>
2013-11-02 00:40:24 +0530

    Changes in main.py

--- main.py
+++ main.py
@@ -6,4 +6,8 @@ import Recorder
 """Surprisingly importing Recorder automatically tells the interpreter to execute Recorder.py as if in a function call"""
 
 s.call(["flac","output.wav","-f"]) #This simply changes the file to a .flac to be used by the google speech api
-win.func('output.flac') #Voila this returns my important response!
+response = win.func('output.flac') #Voila this returns my important response!
+
+index = response.find("utterance")
+print response, index  
+print response[index+12]
