86ef24c112c8cd857ad3eaf765ac00b8dfbe74db
aman <aman@aman.(none)>
2013-11-02 00:55:26 +0530

    Recorder.py

--- Recorder.py
+++ Recorder.py
@@ -5,8 +5,8 @@ import wave
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
 CHANNELS = 2
-RATE = 44100
-RECORD_SECONDS = 5
+RATE = 16000
+RECORD_SECONDS = 2
 WAVE_OUTPUT_FILENAME = "output.wav"
 
 p = pyaudio.PyAudio()
