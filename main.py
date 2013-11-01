03fe7b2aad186c22a43acd79ce1eed9253e93208
aman <aman@aman.(none)>
2013-11-02 00:05:22 +0530

    Added main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import subprocess as s
+import os
+import sys
+import win
+import Recorder 
+"""Surprisingly importing Recorder automatically tells the interpreter to execute Recorder.py as if in a function call"""
+
+s.call(["flac","output.wav","-f"]) #This simply changes the file to a .flac to be used by the google speech api
+win.func('output.flac') #Voila this returns my important response!
