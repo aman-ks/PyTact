b91ffc6f7d165150ed05470099d49c3748d7f632
MarchThe2nd <MarchThe2nd@users.noreply.github.com>
2013-11-02 06:11:35 +0530

    Delete Google.py

--- Google.py
+++
@@ -1,8 +0,0 @@
-import urllib2
-
-url = "https://www.google.com/speech-api/v1/recognize?xjerr=1&client=chromium&lang=en-IN"
-audio = open('output.flac','rb').read()
-headers={'Content-Type': 'audio/x-flac; rate=16000', 'User-Agent':'Mozilla/5.0'}
-request = urllib2.Request(url, data=audio, headers=headers)
-response = urllib2.urlopen(request)
-print response.read()
